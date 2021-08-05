46d8a0651969786e3787a66c2a94b0309519f031
bianca <bianca.ianc@3pillarglobal.com>
2021-08-05 15:06:26 +0300

    Add integration tests

--- src/test/java/com/summer/intern/articlemapper/ArticleControllerTest.java
+++ src/test/java/com/summer/intern/articlemapper/ArticleControllerTest.java
@@ -1,17 +1,22 @@
 package com.summer.intern.articlemapper;
 
 
-import com.summer.intern.articlemapper.controllers.ArticleController;
+
 import com.summer.intern.articlemapper.models.Article;
+import com.summer.intern.articlemapper.repositories.ArticleRepository;
 import com.summer.intern.articlemapper.services.ArticleService;
 import org.junit.jupiter.api.Test;
+
 
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+
 import org.springframework.test.web.servlet.MockMvc;
 
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -21,20 +26,24 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(ArticleController.class)
-
-
+@AutoConfigureMockMvc
+@SpringBootTest(classes = ArticleMapperApplication.class)
 public class ArticleControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
     @MockBean
+    private ArticleRepository articleRepository;
+
+    @Autowired
     private ArticleService articleService;
 
     public static Date parseDate(String date) {
@@ -49,52 +58,52 @@ public class ArticleControllerTest {
     public void getArticles_happy() throws Exception {
         Article providedArticle1 = new Article("aaa", "dsdd", parseDate("2014-02-14"), "bbb");
         Article providedArticle2 = new Article("bbb", "dsdd", parseDate("2014-02-14"), "bbb");
-        when(articleService.getArticles()).thenReturn(Arrays.asList(providedArticle1, providedArticle2));
+        when(articleRepository.findAll()).thenReturn(Arrays.asList(providedArticle1, providedArticle2));
         mockMvc.perform(MockMvcRequestBuilders
                 .get("/getArticles"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(StringUtils.readFileAsString("src/test/resources/get-articles-response.json")));
-        Mockito.verify(articleService, Mockito.times(1)).getArticles();
+        Mockito.verify(articleRepository, Mockito.times(1)).findAll();
     }
 
 
     @Test
     public void getArticle_happy() throws Exception {
         Article providedArticle = new Article("aaa", "dsdd", parseDate("2014-02-14"), "bbb");
-        when(articleService.exist(providedArticle.getId())).thenReturn(true);
-        when(articleService.getArticle(providedArticle.getId())).thenReturn(providedArticle);
+        when(articleRepository.existsById(providedArticle.getId())).thenReturn(true);
+        when(articleRepository.findById(providedArticle.getId())).thenReturn(Optional.of(providedArticle));
         mockMvc.perform(MockMvcRequestBuilders
                 .get("/getArticle/{id}", providedArticle.getId()))
                 .andExpect(status().isOk())
                 .andExpect(content().string(StringUtils.readFileAsString("src/test/resources/get-article-response.json")));
 
-        Mockito.verify(articleService, Mockito.times(1)).exist(providedArticle.getId());
-        Mockito.verify(articleService, Mockito.times(1)).getArticle(providedArticle.getId());
+        Mockito.verify(articleRepository, Mockito.times(1)).existsById(providedArticle.getId());
+        Mockito.verify(articleRepository, Mockito.times(1)).findById(providedArticle.getId());
     }
 
     @Test
     public void getArticle_sad() throws Exception {
         Article providedArticle = new Article("aaa", "dsdd", new Date(), "bbb");
-        when(articleService.exist(providedArticle.getId())).thenReturn(false);
+        when(articleRepository.existsById(providedArticle.getId())).thenReturn(false);
         mockMvc.perform(MockMvcRequestBuilders
                 .get("/getArticle/{id}", providedArticle.getId()))
                 .andExpect(status().isNotFound());
-        Mockito.verify(articleService, Mockito.times(1)).exist(providedArticle.getId());
-        Mockito.verify(articleService, Mockito.times(0)).getArticle(providedArticle.getId());
+        Mockito.verify(articleRepository, Mockito.times(1)).existsById(providedArticle.getId());
+        Mockito.verify(articleRepository, Mockito.times(0)).findById(providedArticle.getId());
     }
 
     @Test
     public void addArticle_happy() throws Exception {
         Article providedArticle = new Article("aaa", "dsdd", new Date(), "bbb");
-        when(articleService.exist(providedArticle.getId())).thenReturn(false);
+        when(articleRepository.existsById(providedArticle.getId())).thenReturn(false);
         mockMvc.perform(MockMvcRequestBuilders
                 .post("/addArticle")
                 .content(StringUtils.asJsonString(providedArticle))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
-        Mockito.verify(articleService, Mockito.times(1)).exist(providedArticle.getId());
-        Mockito.verify(articleService, Mockito.times(1)).addArticle(providedArticle);
+        Mockito.verify(articleRepository, Mockito.times(1)).existsById(providedArticle.getId());
+        Mockito.verify(articleRepository, Mockito.times(1)).save(providedArticle);
 
     }
 
@@ -102,44 +111,44 @@ public class ArticleControllerTest {
     @Test
     public void addArticle_sad() throws Exception {
         Article providedArticle = new Article("aaa", "dsdd", new Date(), "bbb");
-        when(articleService.exist(providedArticle.getId())).thenReturn(true);
+        when(articleRepository.existsById(providedArticle.getId())).thenReturn(true);
         mockMvc.perform(MockMvcRequestBuilders
                 .post("/addArticle")
                 .content(StringUtils.asJsonString(providedArticle))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isMethodNotAllowed());
-        Mockito.verify(articleService, Mockito.times(1)).exist(providedArticle.getId());
-        Mockito.verify(articleService, Mockito.times(0)).addArticle(providedArticle);
+        Mockito.verify(articleRepository, Mockito.times(1)).existsById(providedArticle.getId());
+        Mockito.verify(articleRepository, Mockito.times(0)).save(providedArticle);
     }
 
     @Test
     public void updateArticle_happy() throws Exception {
         Article providedArticle = new Article("aaa", "dsdd", new Date(), "bbb");
-        when(articleService.exist(providedArticle.getId())).thenReturn(true);
+        when(articleRepository.existsById(providedArticle.getId())).thenReturn(true);
         mockMvc.perform(MockMvcRequestBuilders
                 .put("/updateArticle")
                 .content(StringUtils.asJsonString(providedArticle))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isAccepted());
-        Mockito.verify(articleService, Mockito.times(1)).exist(providedArticle.getId());
-        Mockito.verify(articleService, Mockito.times(1)).update(providedArticle);
+        Mockito.verify(articleRepository, Mockito.times(1)).existsById(providedArticle.getId());
+        Mockito.verify(articleRepository, Mockito.times(1)).save(providedArticle);
 
     }
 
     @Test
     public void updateArticle_sad() throws Exception {
         Article providedArticle = new Article("aaa", "dsdd", new Date(), "bbb");
-        when(articleService.exist(providedArticle.getId())).thenReturn(false);
+        when(articleRepository.existsById(providedArticle.getId())).thenReturn(false);
         mockMvc.perform(MockMvcRequestBuilders
                 .put("/updateArticle")
                 .content(StringUtils.asJsonString(providedArticle))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isMethodNotAllowed());
-        Mockito.verify(articleService, Mockito.times(1)).exist(providedArticle.getId());
-        Mockito.verify(articleService, Mockito.times(0)).update(providedArticle);
+        Mockito.verify(articleRepository, Mockito.times(1)).existsById(providedArticle.getId());
+        Mockito.verify(articleRepository, Mockito.times(0)).save(providedArticle);
 
     }
 
@@ -148,33 +157,31 @@ public class ArticleControllerTest {
         mockMvc.perform(MockMvcRequestBuilders
                 .delete("/deleteArticles"))
                 .andExpect(status().isOk());
-        Mockito.verify(articleService, Mockito.times(1)).deleteAll();
+        Mockito.verify(articleRepository, Mockito.times(1)).deleteAll();
 
     }
 
     @Test
     public void deleteArticle_happy() throws Exception {
         Article providedArticle = new Article("aaa", "dsdd", new Date(), "bbb");
-        when(articleService.exist(providedArticle.getId())).thenReturn(true);
+        when(articleRepository.existsById(providedArticle.getId())).thenReturn(true);
         mockMvc.perform(MockMvcRequestBuilders
                 .delete("/deleteArticle/{id}", providedArticle.getId()))
-
                 .andExpect(status().isOk());
-        Mockito.verify(articleService, Mockito.times(1)).exist(providedArticle.getId());
-        Mockito.verify(articleService, Mockito.times(1)).delete(providedArticle.getId());
+        Mockito.verify(articleRepository, Mockito.times(1)).existsById(providedArticle.getId());
+        Mockito.verify(articleRepository, Mockito.times(1)).deleteById(providedArticle.getId());
 
     }
 
     @Test
     public void deleteArticle_sad() throws Exception {
         Article providedArticle = new Article("aaa", "dsdd", new Date(), "bbb");
-        when(articleService.exist(providedArticle.getId())).thenReturn(false);
+        when(articleRepository.existsById(providedArticle.getId())).thenReturn(false);
         mockMvc.perform(MockMvcRequestBuilders
                 .delete("/deleteArticle/{id}", providedArticle.getId()))
                 .andExpect(status().isMethodNotAllowed());
-        Mockito.verify(articleService, Mockito.times(1)).exist(providedArticle.getId());
-        Mockito.verify(articleService, Mockito.times(0)).delete(providedArticle.getId());
-
+        Mockito.verify(articleRepository, Mockito.times(1)).existsById(providedArticle.getId());
+        Mockito.verify(articleRepository, Mockito.times(0)).deleteById(providedArticle.getId());
     }
 
 }
